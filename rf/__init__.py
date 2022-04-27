858afbd93845e87bcab2786f4a2da01eea3369f3
Ricardo S. Jacomini <ricardo.jacomini@jhu.edu>
2022-04-27 12:58:53 -0400

    Added Slurm support

--- rf/__init__.py
+++ rf/__init__.py
@@ -201,7 +201,7 @@ def main():
     parser_sbatch = subparsers.add_parser('sbatch', help='Submits a batch script to Slurm')
     parser_sbatch.add_argument('-v', '--verbose', action='store_true')
     parser_sbatch.add_argument('-r', '--recursive', action='store_true')
-    parser_sbatch.add_argument('-o', '--options', action='store_true')
+    parser_sbatch.add_argument('-o', '--options', action='store_true',  help='sbatch arguments' )
     parser_sbatch.add_argument('node')
     parser_sbatch.set_defaults(func=sbatch)
 
