5e26126fe2e4c4ba3210289d9357912c818eecba
Ricardo Jacomini <ricardo.jacomini@jhu.edu>
2022-04-27 10:34:38 -0400

    Added Slurm support

--- rf/__init__.py
+++ rf/__init__.py
@@ -188,6 +188,7 @@ def main():
     parser_run.add_argument('-v', '--verbose', action='store_true')
     parser_run.add_argument('-r', '--recursive', action='store_true')
     parser_run.add_argument('--container_image', help='Singularity (.sif) or Docker Container')
+    parser_run.add_argument('--sbatch', help='Submits a batch script to Slurm')
     parser_run.add_argument('--volume', help='Bind mount a volume')
     parser_run.add_argument('node')
     parser_run.set_defaults(func=run)
